554a3d014c2031ede8b610d67210b7aabdff4509
Nistor Daniel <nisdan91@gmail.com>
2022-07-09 23:46:06 +0300

    Hardware: reconfigure based on EAGLE pinout

--- WaterMyPlants/include/Hardware.h
+++ WaterMyPlants/include/Hardware.h
@@ -8,13 +8,13 @@
 
 namespace Hardware
 {
-    constexpr uint8_t SOIL_MOISTURE_SENSOR_PIN = A2;
-    constexpr uint8_t LIGHT_SENSOR_PIN = A3;
+    constexpr uint8_t SOIL_MOISTURE_SENSOR_PIN = A0;
+    constexpr uint8_t LIGHT_SENSOR_PIN = A1;
 
     constexpr uint8_t WATER_PUMP_PIN = 8;
-    constexpr uint8_t EXECUTION_BUTTON_PIN = 2;
-    constexpr uint8_t MODE_CHANGER_BUTTON_PIN = 1;
-    constexpr uint8_t MENU_NAVIGATION_BUTTON_PIN = 3;
+    constexpr uint8_t MODE_CHANGER_BUTTON_PIN = 2;
+    constexpr uint8_t EXECUTION_BUTTON_PIN = 3;
+    constexpr uint8_t MENU_NAVIGATION_BUTTON_PIN = 4;
 
     constexpr uint8_t NOKIA_RESET_PIN = 13;
     constexpr uint8_t NOKIA_CHIP_ENABLE_PIN = 12;
